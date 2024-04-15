9e89b48458416a8a3780f4597646b6e150de7b91
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-04-15 15:17:00 -0700

    added check win if condition on guessLetter fn

--- snowman.js
+++ snowman.js
@@ -14,10 +14,10 @@ import { ENGLISH_WORDS } from "./words.js";
 
 class SnowmanLogic {
   constructor(maxWrong = 5) {
-    console.debug("SnowmanLogic", {maxWrong});
+    console.debug("SnowmanLogic", { maxWrong });
 
-    this.answer = this.getSecret();
-    this.guessedLetters = new Set();
+    this.answer = this.getSecret(); //returned as a string
+    this.guessedLetters = new Set(); //putting guesses into a SET
     this.numWrong = 0;
     this.maxWrong = maxWrong;
     this.gameState = "PLAYING"; // or "WON" or "LOST"
@@ -57,7 +57,7 @@ class SnowmanLogic {
   /** Guess letter, update game state, and return t/f if letter correct. */
 
   guessLetter(ltr) {
-    console.debug("guessLetter", {ltr});
+    console.debug("guessLetter", { ltr });
 
     this.guessedLetters.add(ltr);
 
@@ -66,6 +66,31 @@ class SnowmanLogic {
     this.numWrong += isCorrect ? 0 : 1;
 
     // TODO: this should update the gameState attr to "WON" or "LOST"
+    /**
+     * Win = all the letters in the secret Word are revealed &&
+     * guesses are under the allotted guess amount
+     *
+     * call getGuessedWord -- returns a string
+     * if the string has no underscores, then the word is guessed
+     *
+     * if the string contains underscores = no win
+     * if the string contains no underscores and number guesses is less than max, win
+     *
+     * Losing: guesses is greater than maxWrongNum
+     */
+
+    //if the guessed word is = to the answer:
+
+    const currGuessedWord = this.getGuessedWord(); //this is a string of curr word
+
+    //FIXME: check if we need this loop? while numWrong is <= maxWrong... do all this
+    if (this.numWrong <= this.maxWrong) {
+      if (currGuessedWord === this.answer) {
+        this.gameState = "WON";
+      }
+    } else {
+      this.gameState = "LOSE";
+    }
 
     return isCorrect;
   }
